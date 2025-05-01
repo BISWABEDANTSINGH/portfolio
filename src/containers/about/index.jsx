b715e55538e0b277f80c5f7c8693a9d403a83204
BISWABEDANTSINGH <145786264+BISWABEDANTSINGH@users.noreply.github.com>
2025-05-01 23:04:59 +0530

    Add files via upload

---
+++ src/containers/about/index.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
+import { DiApple, DiAndroid } from "react-icons/di";
+import { FaDev, FaDatabase } from "react-icons/fa";
+
+const personalDetails = [
+  {
+    label: "Name",
+    value: "BISWABEDANT SINGH",
+  },
+  {
+    label: "Age",
+    value: "22",
+  },
+  {
+    label: "Address",
+    value: "India",
+  },
+  {
+    label: "Email",
+    value: "biswabedantsingh001@gmail.com",
+  },
+  {
+    label: "Contact No",
+    value: "+91-8917690209",
+  },
+];
+
+const jobSummary =
+  "Computer Science student, with strong programming skills in Java, Python, and C++ Proficient in developing efficient code, solving complex problems, and buildinginteractive software solutions. Passionate about software development and eager tocontribute toinnovative tech projects with a solid foundation in data structures,algorithms, and object-oriented programming.";
+
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <PageHeaderContent
+        headerText="About Me"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="about__content">
+        <div className="about__content__personalWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(-900px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Full Stack Developer</h3>
+            <p>{jobSummary}</p>
+          </Animate>
+
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInformationHeaderText">
+              Personal Information
+            </h3>
+            <ul>
+              {personalDetails.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label}</span>
+                  <span className="value">{item.value}</span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__servicesWrapper">
+          <Animate
+            play
+            duration={1.5}
+            delay={1}
+            start={{
+              transform: "translateX(600px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <div className="about__content__servicesWrapper__innerContent">
+              <div>
+                <FaDev size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiAndroid size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <FaDatabase size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+              <div>
+                <DiApple size={60} color="var( --yellow-theme-main-color)" />
+              </div>
+            </div>
+          </Animate>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,27 @@
+export const data = {
+    experience: [
+
+    ],
+    education: [
+    [
+        {
+          title: "Secondary",
+          subTitle: "MODERN PUBLIC SCHOOL",
+          description:
+            "10th Grade",
+        },
+        {
+          title: "Higher Secondary",
+          subTitle: "GOURI SHANKAR RESIDENTAL ENGLISH MEDIUM SCHOOL",
+          description:
+            "12th Grade in Science",
+        },
+        {
+          title: "B.Tech",
+          subTitle: "DAYANANDA SAGAR UNIVERSITY",
+          description:
+            "Bachelor of technology in computer science",
+        },
+      ],
+    ],
+  };
